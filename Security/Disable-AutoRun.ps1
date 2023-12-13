16f2a807c5416336c380428b962f1cdfc4fd1d2d
cquresphere <72066881+cquresphere@users.noreply.github.com>
2023-12-13 09:06:57 +0100

    Create Disable-AutoRun.ps1
    Creating a script to follow recommendations about Disabling AutoRun for all drives.
    
    "Allowing autoplay to execute may introduce malicious code to a system. Autoplay begins reading from a drive as soon as you insert media in the drive. As a result, the setup file of programs or music on audio media may start. By default, autoplay is disabled on removable drives, such as the floppy disk drive (but not the CD-ROM drive) and on network drives. If you enable this policy, you can also disable autoplay on all drives."

---
+++ Security/Disable-AutoRun.ps1
@@ -0,0 +1,30 @@
+# https://www.stigviewer.com/stig/windows_10/2019-09-25/finding/V-63673
+
+function Test-RegistryValue {
+    param (
+        [parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()]$Path,
+        [parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()]$Value 
+    )    
+    try {    
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null return $true 
+    }    
+    catch {    
+        return $false    
+    }    
+}
+
+if(-not $(Test-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" -Value NoDriveTypeAutoRun)){
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" -Name NoDriveTypeAutoRun -PropertyType Dword -Value 255    
+}
+
+
+# https://www.stigviewer.com/stig/windows_7/2014-04-02/finding/V-22692
+if(-not $(Test-RegistryValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" -Value NoAutorun )){
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" -Name NoAutorun  -PropertyType Dword -Value 1   
+}
+
+if(-not $(Test-RegistryValue -Path "registry::HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Value NoDriveTypeAutoRun )){
+    New-Item -Path "registry::HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\" -Name "Policies"
+    New-Item -Path "registry::HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\" -Name "Explorer"
+    New-ItemProperty -Path "registry::HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name NoDriveTypeAutoRun  -PropertyType Dword -Value 255  -Force
+}
